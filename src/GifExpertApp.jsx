c3b199ab745ed52f9a31d36c67f828524d3c6aaf
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-09 16:35:49 -0400

    Fix: add new components and code cleaned

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,35 +1,26 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
   const onAddCategory = (newCategory) => {
     // Validar que no exista en las catagorias
-    if(categories.includes(newCategory)) return;
+    if (categories.includes(newCategory)) return;
 
     setCategories([newCategory, ...categories]);
   };
 
   return (
     <>
-      {/* Titulo */}
       <h1>GifExpertApp</h1>
 
-      {/* input */}
-      <AddCategory 
-        // setCategories={setCategories} 
-        onNewCategory={ onAddCategory}
-      />
+      <AddCategory onNewCategory={onAddCategory} />
 
-      {/* listado de gif */}
-      {/* <button onClick={onAddCategory}>Agregar</button> */}
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-      </ol>
-      {/* Gif Item */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
