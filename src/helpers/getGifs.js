3bdceaf9d4d200589d50d87e31b7d0e60c3c3d2d
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-10 02:43:28 -0400

    Fix: change http to https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=YoNFQ4xX2d7nVKS4dfNCFCe3hKfJpy85&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=YoNFQ4xX2d7nVKS4dfNCFCe3hKfJpy85&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data = [] } = await resp.json();
 
