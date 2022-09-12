63d6c341a27901af3ce8e40f54308cecdcdbcc30
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-12 14:25:40 -0300

    Fix: testing for GifGridItem

---
+++ test/components/GifItem.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { GifItem } from '../../src/components/GifItem';
+
+describe('Prueba en <GifItem />', () => {
+  const title = 'Naruto';
+  const url = 'https://naruto.com/naruto.jpg';
+
+  test('Debe hacer match con el snapshot', () => {
+    const { container } = render(<GifItem title={title} url={url} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Debe de mostrar la imagen con el URL y el ALT indicado', () => {
+    render(<GifItem title={title} url={url} />);
+    // screen.debug();
+    // expect( screen.getByRole('img').src ).toBe( url )
+    // expect( screen.getByRole('img').alt ).toBe( title )
+
+    const { src, alt } = screen.getByRole('img');
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test('Debe de mostrar el titulo en el componente', () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+});
